be160309f89ae8b9a0a5abf891bc7a5ecd8e6d00
Sumedh Salvi <sumedhsalvi25@gmail.com>
2022-01-26 01:45:03 -0500

    Added comments

--- weather-radar-gateway/gateway.js
+++ weather-radar-gateway/gateway.js
@@ -6,6 +6,7 @@ const PORT = 3000
 app.use(express.json())
 app.use('/', routes)
 
+/* initialize and start server port */
 app.listen(PORT, () => {
     console.log('Gateway has started on port ' + PORT)
 })
--- weather-radar-gateway/routes/index.js
+++ weather-radar-gateway/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
+/* API endpoint returns requsted API response */
 router.all('/:apiName', (req, res) => {
     console.log(req.params.apiName)
     res.send(req.params.apiName + '\n ')
