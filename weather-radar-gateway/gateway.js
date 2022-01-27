f70ac3fac43fba28d6333ec932143ff84024d0f0
Sumedh Salvi <sumedhsalvi25@gmail.com>
2022-01-26 20:06:55 -0500

    Gateway api static route testing using fakeapi

--- weather-radar-gateway/gateway.js
+++ weather-radar-gateway/gateway.js
@@ -6,7 +6,7 @@ const PORT = 3000
 app.use(express.json())
 app.use('/', routes)
 
-/* initialize and start server port */
+/* initialize and start gateway server port */
 app.listen(PORT, () => {
     console.log('Gateway has started on port ' + PORT)
 })
