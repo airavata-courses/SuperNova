74e4f76ccb02582dfefe6001bb601351a4242e21
Sumedh Salvi <sumedhsalvi25@gmail.com>
2022-01-29 18:55:50 -0500

    API Dynamic calls and Registry setup

--- Project1-WeatherRadar/weather-radar-gateway/routes/index.js
+++ Project1-WeatherRadar/weather-radar-gateway/routes/index.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios')
+const registry = require('./registry.json')
 
 /* API endpoint returns requsted API response */
-router.all('/:apiName', (req, res) => {
+router.all('/:apiName/:path', (req, res) => {
     console.log(req.params.apiName)
-    /*res.send(req.params.apiName + '\n ')*/
-    axios.get('http://localhost:3001/' + req.params.apiName).then((response) => {
-        res.send(response.data)
-    })
+    if(registry.services[req.params.apiName]){
+        axios({
+            method: req.method,
+            url: registry.services[req.params.apiName].url + req.params.path,
+            headers: req.headers,
+            data: req.body
+        }).then((response) => {
+            res.send(response.data)
+        })
+    }else {
+        res.send("API Name doesn't exist")
+    }
 })
 
 module.exports = router
