e47ba0341f5338d81498ae395acc0b2c87981a79
richajha012 <90305798+richajha012@users.noreply.github.com>
2022-01-31 04:03:01 -0500

    added user-api functionality

--- Project1-WeatherRadar/user-Api/src/main/java/com/indiana/userApi/WeatherradarApplication.java
+++ Project1-WeatherRadar/user-Api/src/main/java/com/indiana/userApi/WeatherradarApplication.java
@@ -1,13 +1,13 @@
-package com.weatherradar.userApi;
+package com.indiana.userApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserApiApplication {
+public class WeatherradarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserApiApplication.class, args);
+		SpringApplication.run(WeatherradarApplication.class, args);
 	}
 
 }
---
+++ Project1-WeatherRadar/user-Api/src/main/java/com/indiana/userApi/model/UserInfo.java
@@ -0,0 +1,51 @@
+package com.indiana.userApi.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table( name = "user_info" )
+public class UserInfo implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2765339563414172599L;
+
+	@Id
+	private Integer id;
+	
+	@Column
+	private String userEmail;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public UserInfo() {
+	}
+	
+	public UserInfo(Integer id, String email) {
+		super();
+		this.id = id;
+		this.userEmail = email;
+	}
+
+}
