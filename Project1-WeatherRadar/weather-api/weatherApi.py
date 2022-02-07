40bbfc88eff23270e8ca54cb17510096270df7b8
Sumedh Salvi <sumedhsalvi25@gmail.com>
2022-02-07 17:26:48 -0500

    merge

--- Project1-WeatherRadar/weather-api/weatherApi.py
+++ Project1-WeatherRadar/weather-api/weatherApi.py
@@ -47,7 +47,7 @@ async def read_root(radar_id, date):
 
 def setup():
     print('welcome to weather radar api')
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
 if __name__ == '__main__':
