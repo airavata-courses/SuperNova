261249ef9397a411b0c88c91bc8950459a2c6c52
Sumedh Salvi <sumedhsalvi25@gmail.com>
2022-03-29 20:08:36 -0400

    Added topic creation and partitioning

--- Project1-WeatherRadar/weather-cache/topic.js
+++ Project1-WeatherRadar/weather-cache/topic.js
@@ -1,19 +1,38 @@
 const {Kafka} = require("kafkajs")
 
-async function run(){    
+run();
+async function run(){
     try
-    {
+    {        
+        //kafka client connection
         const kafka = new Kafka({
             "clientId": "myapp",
-            "brokers" :["http://localhost:9092"]
+            "brokers" :["localhost:9092"]
         })
+
+        //Admin interface to create topics
         const admin = kafka.admin();
         console.log("Connecting.....")
         await admin.connect()
         console.log("Connected!")
+        
+        //topic creation and partitioning radar names A-M, N-Z 
+        await admin.createTopics({
+            "topics": [{
+                "topic" : "Users",
+                "numPartitions": 2
+            }]
+        })
+        console.log("Created Successfully!")
+        await admin.disconnect();
     }
     catch(ex)
     {
         console.error(`Something bad happened ${ex}`)
     }
+    finally{
+        process.exit(0);
+    }
+
+
 }
