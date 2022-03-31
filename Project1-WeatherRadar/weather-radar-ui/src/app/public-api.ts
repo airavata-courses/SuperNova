a657d955ba8823fc89fa87bc72951ce4d78affed
Sanket Darwante <sanketdarwante6@gmail.com>
2022-03-30 20:43:33 -0400

    Update public-api.ts

--- Project1-WeatherRadar/weather-radar-ui/src/app/public-api.ts
+++ Project1-WeatherRadar/weather-radar-ui/src/app/public-api.ts
@@ -2,15 +2,17 @@
 
 export class PublicApi {
 
-  gatewayURL = 'http://localhost:4300';
-  //gatewayURL = 'http://weather-radar-gateway-app:4300';
-  // weatherApiURL = 'http://localhost:4600';
-  weatherApiURL ='http://weather-radar-api-app:4600';
-  weatherCacheURL = 'http://localhost:4400';
-  //weatherCacheURL = 'http://weather-cache-app:4400';
+  //gatewayURL = 'http://localhost:4300';
+  //weatherApiURL = 'http://localhost:4600';
+  //weatherCacheURL = 'http://localhost:4400';
+  //userApiURL = 'http://localhost:4700';
+  
+  gatewayURL = 'http://weatherapp.twilightparadox.com:30004';
+  weatherApiURL ='http://weatherapp.twilightparadox.com:30002';
+  weatherCacheURL = 'http://weatherapp.twilightparadox.com:30003';
   userApiURL = 'http://weatherapp.twilightparadox.com:30001';
 
-
+  
   // weather service
   weatherPlot= '/weatherApi/plot';
   weatherPlotStatus = '/weatherApi/queryStatus';
