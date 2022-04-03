4eab2f1089d9c3a47b0c7b9e17c67e9fbc67898f
Sanket A Darwante <sanketdarwante6@gmail.com>
2022-04-03 02:37:50 -0400

    Added Kafka msg handler code

---
+++ Project1-WeatherRadar/merra-api/kafka_msg_handler.py
@@ -0,0 +1,74 @@
+import json
+from kafka import KafkaConsumer, KafkaProducer
+import plotting
+
+
+def nexrad_msg_consumer():
+    try:
+        print('Merra Kafka Consumer has started')
+        consumer = KafkaConsumer(
+            'merra_incoming',
+            bootstrap_servers=['localhost:9092'],
+            auto_offset_reset='earliest',
+            enable_auto_commit=True,
+            group_id='test2',
+            value_deserializer=lambda m: json.loads(m.decode('ascii')))
+
+        for message in consumer:
+            radar_id = message.value["radar_id"]
+            date = message.value["date"]
+            data_type = message.value["data_type"]
+
+            if(data_type=='Merra-2'):
+                print("Message Consumed: message_topic:{0}, message_partition:{1}, message_offset:{2} "
+                      "message_key:{3}, data_type:{4} , radar_id:{5}, date:{6}"
+                      .format(message.topic, message.partition,
+                              message.offset, message.key,
+                              data_type, radar_id, date))
+                report_date = date.split('-')
+                plot_file = plotting.plot_reflectivity(radar_id, int(report_date[0]), int(report_date[1]),
+                                                       int(report_date[2]))
+                # produce the required msg
+                kafka_producer(plot_file, data_type, radar_id, date)
+            else:
+                print("Message Discarded: Wrong dataType: message_topic:{0}, message_partition:{1}, message_offset:{2} "
+                      "message_key:{3}, data_type:{4} , radar_id:{5}, date:{6}"
+                      .format(message.topic, message.partition,
+                              message.offset, message.key,
+                              data_type, radar_id, date))
+
+    except Exception as e:
+        print("Exception: kafka_consumer:", e)
+
+
+def kafka_producer(plot_file, data_type, radar_id, date):
+    try:
+        producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+        result = True if plot_file else False
+        if (result):
+            msg = bytes(data_type + "," + radar_id + "," + date + ", PROCESS_DONE,", 'ascii')
+            producer.send('merra_outgoing', msg + plot_file).add_callback(on_send_success).add_errback(
+                on_send_error)
+        else:
+            msg = bytes(data_type + "," + radar_id + "," + date + ", PROCESS_FAIL, ", 'ascii')
+            producer.send('merra_outgoing', msg).add_callback(
+                on_send_success).add_errback(
+                on_send_error)
+    except Exception as e:
+        print("Exception: kafka_producer:", e)
+
+
+def on_send_success(record_metadata, data_type, radar_id, date):
+    print("Message Produced Success: message_topic:{0}, message_partition:{1}, message_offset:{2} "
+          "message_key:{3}, data_type:{4} , radar_id:{5}, date:{6}"
+          .format(record_metadata.topic, record_metadata.partition,
+                  record_metadata.offset, record_metadata.key,
+                  data_type, radar_id, date))
+
+
+def on_send_error(record_metadata, data_type, radar_id, date, excp):
+    print("Message Produced Error: message_topic:{0}, message_partition:{1}, message_offset:{2} "
+          "message_key:{3}, data_type:{4} , radar_id:{5}, date:{6}, exception: {7}"
+          .format(record_metadata.topic, record_metadata.partition,
+                  record_metadata.offset, record_metadata.key,
+                  data_type, radar_id, date, excp))
--- Project1-WeatherRadar/merra-api/merraApi.py
+++ Project1-WeatherRadar/merra-api/merraApi.py
@@ -1,13 +1,13 @@
 from starlette.requests import Request
 from starlette.responses import Response
-
-import merraApi as weatherApi
 from fastapi import FastAPI, HTTPException
 import uvicorn
-from fastapi.responses import FileResponse
 from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
+
+import kafka_msg_handler
 import plotting
+import multiprocessing
 
 app = FastAPI()
 
@@ -34,7 +34,7 @@ async def catch_exceptions_middleware(request: Request, call_next):
 app.middleware('http')(catch_exceptions_middleware)
 
 
-@app.get("/weatherApi/plot")
+@app.get("/merraApi/plot")
 async def read_root(radar_id, date):
     print('radar_id:{0}, date:{1}'.format(radar_id, date))
     report_date = date.split('-')
@@ -45,14 +45,21 @@ async def read_root(radar_id, date):
         raise HTTPException(status_code=404, detail="Item not found")
 
 
-def setup():
-    print('welcome to weather radar api')
-    uvicorn.run(app, host="0.0.0.0", port=4600)
+@app.get("/merraApi/buildinfo")
+async def read_buildinfo():
+    return 'merra-api-00'
 
+
+def setup():
+    print('welcome to merra api')
+    uvicorn.run(app, host="0.0.0.0", port=4800)
 
 
 if __name__ == '__main__':
-    weatherApi.setup()
+    p1 = multiprocessing.Process(target=setup, args=())
+    p2 = multiprocessing.Process(target=kafka_msg_handler.nexrad_msg_consumer, args=())
+    p1.start()
+    p2.start()
 
 ## Run via docker
 
