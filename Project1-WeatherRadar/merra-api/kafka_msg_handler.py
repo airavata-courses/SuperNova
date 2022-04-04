65ed149254a5a0a956380c08d94582d23d3fb406
Sanket A Darwante <sanketdarwante6@gmail.com>
2022-04-04 00:54:22 -0400

    Fixed kafka config, loogs and added ironman.gif temp for testing

--- Project1-WeatherRadar/merra-api/kafka_msg_handler.py
+++ Project1-WeatherRadar/merra-api/kafka_msg_handler.py
@@ -8,10 +8,10 @@ def nexrad_msg_consumer():
         print('Merra Kafka Consumer has started')
         consumer = KafkaConsumer(
             'merra_incoming',
-            bootstrap_servers=['localhost:9092'],
+            bootstrap_servers=['kafka-0.kafka-headless.space-dev.svc.cluster.local:9092'],
             auto_offset_reset='earliest',
             enable_auto_commit=True,
-            group_id='test2',
+            group_id='Merra-2_incoming_group',
             value_deserializer=lambda m: json.loads(m.decode('ascii')))
 
         for message in consumer:
@@ -19,8 +19,8 @@ def nexrad_msg_consumer():
             date = message.value["date"]
             data_type = message.value["data_type"]
 
-            if(data_type=='Merra-2'):
-                print("Message Consumed: message_topic:{0}, message_partition:{1}, message_offset:{2} "
+            if data_type == 'Merra-2':
+                print("Merra Consumer Message Consumed: message_topic:{0}, message_partition:{1}, message_offset:{2} "
                       "message_key:{3}, data_type:{4} , radar_id:{5}, date:{6}"
                       .format(message.topic, message.partition,
                               message.offset, message.key,
@@ -31,21 +31,22 @@ def nexrad_msg_consumer():
                 # produce the required msg
                 kafka_producer(plot_file, data_type, radar_id, date)
             else:
-                print("Message Discarded: Wrong dataType: message_topic:{0}, message_partition:{1}, message_offset:{2} "
+                print("Merra Consumer Message Discarded: Wrong dataType: message_topic:{0}, message_partition:{1}, "
+                      "message_offset:{2} "
                       "message_key:{3}, data_type:{4} , radar_id:{5}, date:{6}"
                       .format(message.topic, message.partition,
                               message.offset, message.key,
                               data_type, radar_id, date))
 
     except Exception as e:
-        print("Exception: kafka_consumer:", e)
+        print("Merra Consumer Exception:", e)
 
 
 def kafka_producer(plot_file, data_type, radar_id, date):
     try:
-        producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+        producer = KafkaProducer(bootstrap_servers=['kafka-0.kafka-headless.space-dev.svc.cluster.local:9092'])
         result = True if plot_file else False
-        if (result):
+        if result:
             msg = bytes(data_type + "," + radar_id + "," + date + ", PROCESS_DONE,", 'ascii')
             producer.send('merra_outgoing', msg + plot_file).add_callback(on_send_success).add_errback(
                 on_send_error)
@@ -55,11 +56,11 @@ def kafka_producer(plot_file, data_type, radar_id, date):
                 on_send_success).add_errback(
                 on_send_error)
     except Exception as e:
-        print("Exception: kafka_producer:", e)
+        print("Merra Producer Exception:", e)
 
 
 def on_send_success(record_metadata, data_type, radar_id, date):
-    print("Message Produced Success: message_topic:{0}, message_partition:{1}, message_offset:{2} "
+    print("Merra Producer Message Produced Success: message_topic:{0}, message_partition:{1}, message_offset:{2} "
           "message_key:{3}, data_type:{4} , radar_id:{5}, date:{6}"
           .format(record_metadata.topic, record_metadata.partition,
                   record_metadata.offset, record_metadata.key,
@@ -67,7 +68,7 @@ def on_send_success(record_metadata, data_type, radar_id, date):
 
 
 def on_send_error(record_metadata, data_type, radar_id, date, excp):
-    print("Message Produced Error: message_topic:{0}, message_partition:{1}, message_offset:{2} "
+    print("Merra Producer Message Produced Error: message_topic:{0}, message_partition:{1}, message_offset:{2} "
           "message_key:{3}, data_type:{4} , radar_id:{5}, date:{6}, exception: {7}"
           .format(record_metadata.topic, record_metadata.partition,
                   record_metadata.offset, record_metadata.key,
