dfb0b6db661c7c10cb7c8384ab23ce4a62389e6a
richajha012 <90305798+richajha012@users.noreply.github.com>
2022-04-10 03:13:58 -0400

    Fixed message exception handling

--- Project1-WeatherRadar/merra-api/kafka_msg_handler.py
+++ Project1-WeatherRadar/merra-api/kafka_msg_handler.py
@@ -1,4 +1,6 @@
 import json
+import traceback
+
 from kafka import KafkaConsumer, KafkaProducer
 import plotting
 
@@ -6,6 +8,7 @@ import plotting
 def merra_msg_consumer():
     try:
         print('Merra Kafka Consumer has started')
+        # kafka-0.kafka-headless.space-dev.svc.cluster.local
         consumer = KafkaConsumer(
             'merra_incoming',
             bootstrap_servers=['kafka-0.kafka-headless.space-dev.svc.cluster.local:9092'],
@@ -15,9 +18,9 @@ def merra_msg_consumer():
             value_deserializer=lambda m: json.loads(m.decode('ascii')))
 
         for message in consumer:
-            radar_id = message.value["radar_id"]
-            date = message.value["date"]
-            data_type = message.value["dataType"]
+            radar_id = message.value.get("radar_id")
+            date = message.value.get("date")
+            data_type = message.value.get("dataType")
 
             if data_type == 'Merra-2':
                 print("Merra Consumer Message Consumed: message_topic:{0}, message_partition:{1}, message_offset:{2} "
@@ -26,8 +29,7 @@ def merra_msg_consumer():
                               message.offset, message.key,
                               data_type, radar_id, date))
                 report_date = date.split('-')
-                plot_file = plotting.plot_reflectivity(radar_id, int(report_date[0]), int(report_date[1]),
-                                                       int(report_date[2]))
+                plot_file = plotting.plot_merra(radar_id, report_date[0], report_date[1], report_date[2])
                 # produce the required msg
                 kafka_producer(plot_file, data_type, radar_id, date)
             else:
@@ -40,43 +42,45 @@ def merra_msg_consumer():
 
     except Exception as e:
         print("Merra Consumer Exception:", e)
+        traceback.print_exc()
 
 
 def kafka_producer(plot_file, data_type, radar_id, date):
     try:
+        print('Producing resultant output:')
+        # kafka-0.kafka-headless.space-dev.svc.cluster.local
         producer = KafkaProducer(bootstrap_servers=['kafka-0.kafka-headless.space-dev.svc.cluster.local:9092'])
         result = True if plot_file else False
         if result:
+            print('Producing result: True')
             msg = bytes(data_type + "," + radar_id + "," + date + ",PROCESS_DONE,", 'ascii')
-            producer.send('merra_outgoing', msg + plot_file).add_callback(on_send_success).add_errback(
-                on_send_error)
+            producer.send('merra_outgoing', msg + plot_file)\
+                .add_callback(on_send_success,)\
+                .add_errback(on_send_error)
+
         else:
+            print('Producing result: False')
             msg = bytes(data_type + "," + radar_id + "," + date + ",PROCESS_FAIL, ", 'ascii')
-            producer.send('merra_outgoing', msg).add_callback(
-                on_send_success).add_errback(
-                on_send_error)
+            producer.send('merra_outgoing', msg).add_callback(on_send_success).add_errback(on_send_error)
     except Exception as e:
         print("Merra Producer Exception:", e)
+        traceback.print_exc()
+    print('Producer work done.')
 
 
-def on_send_success(record_metadata, data_type, radar_id, date):
-    print("Merra Producer Message Produced Success: message_topic:{0}, message_partition:{1}, message_offset:{2} "
-          "message_key:{3}, data_type:{4} , radar_id:{5}, date:{6}"
-          .format(record_metadata.topic, record_metadata.partition,
-                  record_metadata.offset, record_metadata.key,
-                  data_type, radar_id, date))
+def on_send_success(record_metadata):
+    print("Merra Producer Message Produced Success")
+    print(record_metadata)
 
 
-def on_send_error(record_metadata, data_type, radar_id, date, excp):
-    print("Merra Producer Message Produced Error: message_topic:{0}, message_partition:{1}, message_offset:{2} "
-          "message_key:{3}, data_type:{4} , radar_id:{5}, date:{6}, exception: {7}"
-          .format(record_metadata.topic, record_metadata.partition,
-                  record_metadata.offset, record_metadata.key,
-                  data_type, radar_id, date, excp))
+def on_send_error(excp):
+    print("Merra Producer Message Produced Error")
+    print(excp)
 
 
-if __name__ == '__main__':
-    try:
-        merra_msg_consumer()
-    except Exception as e:
-        print("Error: unable to start thread", e)
+# if __name__ == '__main__':
+#     try:
+#         merra_msg_consumer()
+#     except Exception as e:
+#         print("Error: unable to start ", e)
+#         traceback.print_exc()
